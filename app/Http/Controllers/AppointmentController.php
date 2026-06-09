d728b5fd379929c0871f248a6c0147ae15a7ebc7
JustASimpleMahdi <just.a.simple.mahdi@gmail.com>
2026-06-09 22:26:07 +0330

    add date to redirect for create and update appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -71,7 +71,7 @@ class AppointmentController extends Controller
         ]);
         $appointment->personnels()->sync($validated['personnels']);
 
-        return redirect()->route('manager.appointments.index');
+        return redirect()->route('manager.appointments.index', ['date' => $validated['date']]);
     }
 
     /**
@@ -134,7 +134,7 @@ class AppointmentController extends Controller
             'end' => $endDate,
         ]);
         $appointment->personnels()->sync($validated['personnels']);
-        return redirect()->route('manager.appointments.index');
+        return redirect()->route('manager.appointments.index', ['date' => $validated['date']]);
     }
 
     /**
